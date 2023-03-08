8da69000c2000108926771cc0366d7b69f08d4eb
Merrick Luo <merrick@luois.me>
2023-03-08 19:28:33 +0800

    feat: allow refresh summary

--- src/pages/popup/Main.tsx
+++ src/pages/popup/Main.tsx
@@ -1,9 +1,13 @@
 import { ParseDocumentCommand } from "@src/lib/constants";
 import { Article } from "@src/lib/readbility";
-import { tabs } from "webextension-polyfill";
+import { runtime, tabs } from "webextension-polyfill";
 import { SiOrg } from "react-icons/si";
 import { useQuery } from "react-query";
 import Summary from "./Summary";
+import { BiRefresh } from "react-icons/bi";
+import { useMemo } from "react";
+import { loadSettings } from "@src/lib/settings";
+import { selectedSummarizer } from "@src/lib/summarizer";
 
 const fetchArticle = async (): Promise<Article> => {
   const tab = await tabs.query({
@@ -22,24 +26,50 @@ const fetchArticle = async (): Promise<Article> => {
   return await tabs.sendMessage(tab[0].id, ParseDocumentCommand);
 };
 
+const fetchSummary = async (article: Article) => {
+  // FIXME: don't understand why error all become Unexpected
+  // is it not a Promise???
+  return await runtime.sendMessage({
+    action: "summarize",
+    title: article.title,
+    content: article.textContent,
+  });
+};
+
 const Main = () => {
   const article = useQuery("article", fetchArticle);
+  const { data: settings } = useQuery("settings", loadSettings);
+
+  const hasSelectedSummarizer = useMemo(() => {
+    return settings && !!selectedSummarizer(settings);
+  }, [settings]);
+
+  const summary = useQuery("summary", () => fetchSummary(article.data!), {
+    enabled: !!article.data && hasSelectedSummarizer,
+  });
 
   return (
     <div className="p-3">
       <section>
-        <h1 className="font-bold ml-1 mt-1">
-          {article.isLoading
-            ? "Parsing page..."
-            : article.isError
-            ? "Failed to parse page."
-            : article.data?.title}
-        </h1>
+        <div className="flex justify-between items-end">
+          <h1 className="font-bold ml-1 mt-1 max-w-xs">
+            {article.isLoading
+              ? "Parsing page..."
+              : article.isError
+              ? "Failed to parse page."
+              : article.data?.title}
+          </h1>
+          <BiRefresh
+            onClick={() => summary.refetch()}
+            className={`w-6 h-6 hover:text-blue-600 mr-2 ${
+              summary.isFetching ? "animate-spin" : ""
+            }`}
+          />
+        </div>
         <div className="w-full bg-white mt-2 p-2 rounded-lg border-1 border-gray-300">
-          <Summary article={article.data} />
+          <Summary summary={summary} valid={!!hasSelectedSummarizer} />
         </div>
       </section>
-
       <div className="mt-6 mb-3 mr-3 flex justify-end items-center">
         <p className="text-sm text-gray-300">Save clip to...</p>
         <SiOrg className="w-6 h-6 ml-3" />
--- src/pages/popup/Summary.tsx
+++ src/pages/popup/Summary.tsx
@@ -1,34 +1,14 @@
-import { useQuery } from "react-query";
-import { Article } from "@src/lib/readbility";
-import { runtime } from "webextension-polyfill";
-import { loadSettings } from "@src/lib/settings";
-import { selectedSummarizer } from "@src/lib/summarizer";
-import { useMemo } from "react";
+import { UseQueryResult } from "react-query";
 
-const fetchSummary = (article: Article) => {
-  // FIXME: don't understand why error all become Unexpected
-  // is it not a Promise???
-  return async (): Promise<string> => {
-    return await runtime.sendMessage({
-      action: "summarize",
-      title: article.title,
-      content: article.textContent,
-    });
-  };
-};
-
-const Summary = ({ article }: { article: Article | undefined }) => {
-  const { data: settings } = useQuery("settings", loadSettings);
-
-  const hasSelectedSummarizer = useMemo(() => {
-    return settings && !!selectedSummarizer(settings);
-  }, [settings]);
-
-  const { data, error } = useQuery("summary", fetchSummary(article!), {
-    enabled: !!article && hasSelectedSummarizer,
-  });
-
-  if (!hasSelectedSummarizer) {
+const Summary = ({
+  summary,
+  valid,
+}: {
+  summary: UseQueryResult<string>;
+  valid: boolean;
+}) => {
+  const { error, data } = summary;
+  if (!valid) {
     return <p>No summary provider selected, please check the settings.</p>;
   }
 
