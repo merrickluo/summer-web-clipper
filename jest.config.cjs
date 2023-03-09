de67491bb835fe1ab533880697d523defa1a697c
Merrick Luo <merrick@luois.me>
2023-03-09 18:40:23 +0800

    fix: jest resolve modules

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+
+const { pathsToModuleNameMapper } = require("ts-jest");
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require("./tsconfig");
+
 module.exports = {
   preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+  roots: ["<rootDir>"],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
