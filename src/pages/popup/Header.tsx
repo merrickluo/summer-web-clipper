bff35193b09580753f8a52be7dcf28e8584ef21b
Merrick Luo <merrick@luois.me>
2023-03-10 09:24:36 +0800

    feat: add robot icon

--- src/pages/popup/Header.tsx
+++ src/pages/popup/Header.tsx
@@ -1,6 +1,8 @@
 import { GiEclipseFlare } from "react-icons/gi";
 import { ReactNode } from "react";
 
+import Icon from "@assets/Icon";
+
 interface HeaderProps {
   subtitle: string;
   actions: ReactNode | ReactNode[];
@@ -10,7 +12,7 @@ const Header = ({ subtitle, actions }: HeaderProps) => {
   return (
     <header className="bg-white h-16 w-full flex items-center border-b border-gray-300">
       <div className="flex ml-3 items-center w-full">
-        <GiEclipseFlare className="w-8 h-8 text-orange-300" />
+        <Icon className="w-10 h-10" />
         <div className="ml-2">
           <span className="text-lg">Summber Web Clipper</span>
           <span className="text-xs ml-2">0.0.1</span>
