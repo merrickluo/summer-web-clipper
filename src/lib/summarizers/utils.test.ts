3575033bb6da77cb1d012f69487675a581388be8
ahxxm <ahxxm@users.noreply.github.com>
2023-06-17 11:29:19 +0900

    use 16k model when more than 3000 words (#22)
    * threshold description to use 16k context model
    
    * use 16k model when more than 3000 words
    
    * truncate by word limit
    
    * add sanitizeContent test

--- src/lib/summarizers/utils.test.ts
+++ src/lib/summarizers/utils.test.ts
@@ -4,4 +4,9 @@ describe("sanitize string", () => {
   test("replace multiple spaces to one", () => {
     expect(sanitizeContent("a   b  c\n\nd")).toBe("a b c d");
   });
+
+  test("truncate to first K words split by spaces", () => {
+    expect(sanitizeContent("a   b  c\n\nd", 2)).toBe("a b");
+    expect(sanitizeContent("a   b  c\n\nd", 4)).toBe("a b c d");
+  });
 });
--- src/lib/summarizers/utils.ts
+++ src/lib/summarizers/utils.ts
@@ -1,8 +1,5 @@
 export const sanitizeContent = (content: string, limit = 0): string => {
-  const text = content.replaceAll(/\s+/g, " ");
-  if (limit != 0) {
-    return text.slice(0, limit);
-  }
-
-  return text;
+  const textSlices = content.replaceAll(/\s+/g, " ").split(" ");
+  const truncateSlices = limit === 0 ? textSlices : textSlices.slice(0, limit);
+  return truncateSlices.join(" ");
 };
