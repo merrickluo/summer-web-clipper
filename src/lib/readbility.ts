0e9a389717c833b71d44f52e0c5cd951bf7831cb
Merrick Luo <merrick@luois.me>
2024-10-20 21:27:07 +0800

    feat: use youtube transcript for summary (#33)
    * feat: use youtube transcript for summary
    
    * docs: fix typo in comments and remove unused comment

--- src/lib/readbility.ts
+++ src/lib/readbility.ts
@@ -1,4 +1,5 @@
 import { isProbablyReaderable, Readability } from "@mozilla/readability";
+import { YoutubeTranscript } from "youtube-transcript";
 
 export interface Doc {
   title: string;
@@ -13,11 +14,16 @@ export interface Doc {
   url: string;
   language: string;
 
+  youtube?: boolean; // indicate document is a youtube video
   summary?: string;
 }
 
 // this needs to be run in content script
-export const parseDocument = async (): Promise<Doc> => {
+export const parseDocument = async (isYoutube): Promise<Doc> => {
+  if (isYoutube) {
+    return getYoutubeTranscript();
+  }
+
   const readable = isProbablyReaderable(document);
   const cloneDoc = document.cloneNode(true) as Document;
   const parsedDoc = new Readability(cloneDoc).parse();
@@ -35,4 +41,26 @@ export const parseDocument = async (): Promise<Doc> => {
   }
 
   return { url: window.location.href, readable, language, ...parsedDoc };
+};
+
+const getYoutubeTranscript = async (): Promise<Doc> => {
+  const resp = await YoutubeTranscript.fetchTranscript(window.location.href);
+  if (resp.length <= 0) {
+    throw new Error("transcript not found");
+  }
+
+  const language = resp[0].lang || "en";
+  const textContent = resp.map((it) => it.text).join(" ");
+
+  console.debug("got Youtube transcript: ", textContent);
+
+  // TODO: maybe fill other attributes. right now only language, title, textContent is used.
+  return {
+    url: window.location.href,
+    readable: true,
+    youtube: true,
+    title: document.title,
+    language,
+    textContent,
+  };
 };
--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,15 +1,23 @@
 import { addMessageListener, ContentMessage } from "@lib/browser";
 import { parseDocument } from "@lib/readbility";
 
-addMessageListener((msg: ContentMessage, _, respond) => {
-  switch (msg.action) {
-    case "parse_document":
-      parseDocument().then((doc) => {
-        respond({ type: "success", payload: doc });
-      });
-      return true;
-    case "open_url":
-      window.open(msg.payload as string, "_self");
-      return true;
-  }
-});
+if (!window.contentInjected) {
+  window.contentInjected = true;
+
+  const RE_YOUTUBE =
+    /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/i;
+  const isYoutube = window.location.href.match(RE_YOUTUBE);
+
+  addMessageListener((msg: ContentMessage, _, respond) => {
+    switch (msg.action) {
+      case "parse_document":
+        parseDocument(isYoutube).then((doc) => {
+          respond({ type: "success", payload: doc });
+        });
+        return true;
+      case "open_url":
+        window.open(msg.payload as string, "_self");
+        return true;
+    }
+  });
+}
