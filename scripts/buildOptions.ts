3c85cdfc3aefa5f0eff102275d603ae4f7f646b6
Merrick Luo <merrick@luois.me>
2024-10-26 22:15:43 +0800

    chore: do not build popup

--- scripts/buildOptions.ts
+++ scripts/buildOptions.ts
@@ -5,7 +5,7 @@ import stylePlugin from "esbuild-style-plugin";
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 
-const pages = ["popup", "content", "background"];
+const pages = ["content", "background"];
 const files = ["index.tsx", "index.ts"];
 
 export function loadBuildOptions(debug = false): BuildOptions[] {
