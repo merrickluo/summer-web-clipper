83b8c60b835d4f0870ff79ab593b5194d2bf80a9
Merrick Luo <merrick@luois.me>
2024-10-26 22:25:32 +0800

    chore: make header rounded at top

--- src/pages/popup/Header.tsx
+++ src/pages/popup/Header.tsx
@@ -10,7 +10,7 @@ interface HeaderProps {
 
 const Header = ({ subtitle, actions }: HeaderProps) => {
   return (
-    <header className="bg-white h-16 w-full flex items-center border-b border-gray-300">
+    <header className="bg-white rounded-t-lg h-16 w-full flex items-center border-b border-gray-300">
       <div className="flex ml-3 items-center w-full">
         <Icon className="w-10 h-10" />
         <div className="ml-2">
