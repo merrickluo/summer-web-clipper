c1c4efb4c21f1bf906efb4b4c94c62c61a596b0f
Merrick Luo <merrick@luois.me>
2024-11-01 13:00:09 +0800

    chore: can use rem again with iframe

--- scripts/build_options.ts
+++ scripts/build_options.ts
@@ -32,7 +32,6 @@ export function loadBuildOptions(debug = false): BuildOptions[] {
               require("postcss-import"),
               require("tailwindcss"),
               require("autoprefixer"),
-              require("@thedutchcoder/postcss-rem-to-px"),
             ],
           },
         }),
