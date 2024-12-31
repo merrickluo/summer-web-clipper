8e7a089722e7539600067f9386fb65203a6a4a55
ahxxm <i@ahxxm.com>
2024-12-31 11:24:16 +0000

    bump groq llama 3.3

--- src/components/settings/summarizers/openai.tsx
+++ src/components/settings/summarizers/openai.tsx
@@ -70,8 +70,7 @@ const OpenAISettings = ({ settings, dispatch }: SettingsFormProps) => {
           {[
             "gpt-4o-mini",
             "gpt-4o",
-            "llama-3.1-8b-instant",
-            "llama-3.1-70b-versatile",
+            "llama-3.3-70b-versatile",
             "mistral-large-latest",
           ].map((model) => (
             <option key={model} value={model}>
