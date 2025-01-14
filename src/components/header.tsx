326fb3686ca68123008bd63a6fa40e6f983b49fd
Merrick Luo <merrick@luois.me>
2025-01-14 21:42:30 +0800

    chore: add padding to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ interface HeaderProps {
 const Header = ({ subtitle, actions }: HeaderProps) => {
   return (
     // Certain websites injects CSS for `header` tags, had to use a `div` instead.
-    <div className="swc-bg-white swc-rounded-t-lg swc-flex swc-items-center swc-border-b swc-border-gray-300">
+    <div className="swc-bg-white swc-rounded-t-lg swc-flex swc-items-center swc-border-b swc-border-gray-300 swc-py-2">
       <div className="swc-flex swc-ml-3 swc-items-center swc-w-full">
         <Icon className="swc-w-10 swc-h-10" />
         <div className="swc-flex swc-flex-col swc-ml-2">
