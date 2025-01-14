a9d88096cf3e7b5941524203a13b63478e7277fc
Merrick Luo <merrick@luois.me>
2025-01-14 21:52:20 +0800

    feat: enable preflight again

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   corePlugins: {
-    preflight: false,
+    preflight: true,
   },
   content: ["./src/**/*.tsx", "./public/**/*.html"],
   theme: {
