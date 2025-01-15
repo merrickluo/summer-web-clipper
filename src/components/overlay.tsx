1b15fe759fb24e73a8214d93b1d27224e9fbe1a5
Merrick Luo <merrick@luois.me>
2025-01-15 13:50:10 +0800

    chore: type def for overlay props

--- src/components/overlay.tsx
+++ src/components/overlay.tsx
@@ -1,6 +1,12 @@
-const Overlay = ({ children }) => {
+import { ReactElement } from "react";
+
+interface OverlayProps {
+  children: ReactElement | ReactElement[];
+}
+
+const Overlay = ({ children }: OverlayProps) => {
   return (
-    <div className="swc-fixed swc-z-999 swc-bottom-2 swc-right-2 swc-h-3/5 swc-w-1/3 swc-rounded-lg swc-border swc-border-gray-300 swc-bg-gray-50 swc-shadow">
+    <div className="swc-preflight swc-fixed swc-z-999 swc-bottom-2 swc-right-2 swc-h-3/5 swc-w-1/3 swc-rounded-lg swc-border swc-border-gray-300 swc-bg-gray-50 swc-shadow">
       {children}
     </div>
   );
