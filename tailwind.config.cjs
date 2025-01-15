31a07b37ff4a8605a613618484a60e95732f3abd
Merrick Luo <merrick@luois.me>
2025-01-15 13:50:34 +0800

    feat: only apply preflight in swc

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
+const plugin = require("tailwindcss/plugin");
+const fs = require("fs");
+const path = require("path");
+const postcss = require("postcss");
 
+// preflight is disabled because we inject css to the page
+// using preflight will affect page display.
+// instead use a custom plugin to apply preflight only in containers for the extension
+// see: https://github.com/tailwindlabs/tailwindcss/discussions/10332
 module.exports = {
   corePlugins: {
-    preflight: true,
+    preflight: false,
   },
   content: ["./src/**/*.tsx", "./public/**/*.html"],
   theme: {
@@ -21,6 +29,23 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    plugin(({ addBase }) => {
+      const preflightStyles = postcss.parse(
+        fs.readFileSync(
+          require.resolve("tailwindcss/lib/css/preflight.css"),
+          "utf8"
+        )
+      );
+
+      // Scope the selectors to specific components
+      preflightStyles.walkRules((rule) => {
+        rule.selector = ".swc-preflight " + rule.selector;
+      });
+
+      addBase(preflightStyles.nodes);
+    }),
+  ],
   prefix: "swc-",
 };
