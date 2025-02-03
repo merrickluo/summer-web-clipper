9c3dd04bb1d281c223b737619acc1f16283141b3
Merrick Luo <merrick@luois.me>
2025-02-03 13:10:42 +0800

    chroe: add comment about developer prompt

--- src/lib/summarizers/utils.ts
+++ src/lib/summarizers/utils.ts
@@ -1,5 +1,8 @@
 // https://www.reddit.com/r/ChatGPTPro/comments/13n55w7/highly_efficient_prompt_for_summarizing_gpt4/
 // https://www.linkedin.com/pulse/why-llms-terrible-summarizing-alex-turkhanov-mmyue/
+// TODO: replace with developer prompt?
+// Developer-provided instructions that the model should follow, regardless of messages sent by the user. With o1 models and newer, developer messages replace the previous system messages.
+
 export const systemPrompt = (
   language: string
 ) => `As a professional summarizer, create a concise and comprehensive summary of the provided text, be it an article, post, conversation, or passage, while adhering to these guidelines:
