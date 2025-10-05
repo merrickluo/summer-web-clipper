caed88a0aa301150eb9840d1fa228f485f1d4805
Merrick Luo <merrick@luois.me>
2025-10-05 17:46:16 +0800

    feat: daisyui v5 for exporter settings

--- src/components/settings/export_settings.tsx
+++ src/components/settings/export_settings.tsx
@@ -3,37 +3,37 @@ import { availableExporters, findExporter } from "@lib/exporters";
 import { SyntheticEvent, useCallback, useState } from "react";
 
 const ExportSettings = ({ settings, dispatch }: SettingsFormProps) => {
-  const [selected, setSelected] = useState(availableExporters[0]);
+  const [selected, setSelected] = useState(undefined);
 
   const handleSelectExporter = useCallback(
     (event: SyntheticEvent<HTMLSelectElement>) => {
-      const exporter =
-        findExporter(event.currentTarget.value) || availableExporters[0];
+      const exporter = findExporter(event.currentTarget.value);
       setSelected(exporter);
     },
-    [setSelected]
+    [setSelected],
   );
 
   return (
-    <div className="swc:form-control swc:w-full">
-      <label className="swc:label">
-        <span className="swc:text-secondary swc:text-xs swc:label-text">
-          Exporter
-        </span>
-      </label>
-      <select
-        id="exporters"
-        className="swc:select swc:select-bordered swc:w-full"
-        onChange={handleSelectExporter}
-        defaultValue={selected?.id}
-      >
-        <option key="emtpy">Select exporter</option>
-        {availableExporters.map(({ id, name }) => (
-          <option key={id} value={id}>
-            {name}
-          </option>
-        ))}
-      </select>
+    <div className="swc:w-full">
+      <fieldset className="swc:fieldset swc:w-full">
+        <label className="swc:input swc:w-full">
+          <span className="swc:label">Exporter</span>
+          <select
+            id="exporters"
+            onChange={handleSelectExporter}
+            className="swc:w-full"
+            defaultValue={selected?.id}
+          >
+            <option key="emtpy">Select exporter</option>
+            {availableExporters.map(({ id, name }) => (
+              <option key={id} value={id}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </label>
+      </fieldset>
+
       {selected && (
         <selected.SettingsComp settings={settings} dispatch={dispatch} />
       )}
